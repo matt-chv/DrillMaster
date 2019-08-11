c43ff251fb8637ea325faf1854ebcdac6fbb97ef
matt-chv <matthieu_chevrier@hotmail.com>
2019-08-11 15:37:26 +0200

    hsk1

---
+++ static/cards/hsk1.js
@@ -0,0 +1,27 @@
+var cards = [{
+    frontValue: '我',
+    frontHint:  'wǒ',
+    backValue:  'I, me',
+    backHint:   ''
+  }, {
+    frontValue: '我们',
+    frontHint:  'wǒmen',
+    backValue:  'we, us (pl.)',
+    backHint:   ''
+  }, {
+    frontValue: '你',
+    frontHint:  'nǐ',
+    backValue:  'you',
+    backHint:   ''
+  }, {
+    frontValue: '你们',
+    frontHint:  'nǐmen',
+    backValue:  'you (pl.)',
+    backHint:   ''
+  }, {
+    frontValue: '他',
+    frontHint:  'tā',
+    backValue:  'he, him',
+    backHint:   ''
+  }
+]
