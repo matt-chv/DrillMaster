c2d7218ffe3f84f40cfb04e57ee1432b8bccdd3f
matthieu chevrier <contact@matthieuchevrier.com>
2020-09-19 22:06:18 +0200

    Create get_deck.js

---
+++ static/js/get_deck.js
@@ -0,0 +1,23 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "/drillpractice/deck?id=1";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        $("div").data("deck",myArr)
+        
+        show_card();
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function show_card() {
+  arr = $("div").data("deck")
+    var out = "";
+    var card_index;
+    card_index=0;
+    out = arr[card_index]["question"]
+    $("div").data("card",card_index)
+    document.getElementById("question").innerHTML = out;
+}
