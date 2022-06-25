873d5c481057d4d2093733082d2cc43e2ee507aa
matthieu chevrier <contact@matthieuchevrier.com>
2022-06-25 15:48:03 +0200

    Update my_app.py
    removed debug code

--- my_app.py
+++ my_app.py
@@ -166,7 +166,6 @@ def favicon():
 @api.route('/')
 @api.route('/index')
 def index():
-    logging.error("went throug index")
     return api.send_static_file('index.html')
     #return index_html
 
